c11f95256c4024ae6b020d715d6d281830c12cc1
Noah  Klem <klem.noah@gmail.com>
2021-05-31 21:37:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = {
-    name = "noah",
-    age = 35,
-    weight = 145
+    name => "noah",
+    age => 35,
+    weight => 145
   }
   
 end
