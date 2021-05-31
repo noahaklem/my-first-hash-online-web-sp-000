20e91348bbfd170fbd2c244e810e89125f2b9528
Noah  Klem <klem.noah@gmail.com>
2021-05-31 21:44:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,7 +29,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -40,12 +40,13 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
+shipping_manifest["musket"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-
+ return shipping_manifest
 end
